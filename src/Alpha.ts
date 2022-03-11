514f63b2e57a9f78329a769df049fa7f68b09f27
Jesse Kelly <kellyjesse881@gmail.com>
2022-03-11 13:08:51 -0600

    chore(docs): added docs for Alpha

--- src/Alpha.ts
+++ src/Alpha.ts
@@ -1,11 +1,27 @@
 import * as t from "io-ts";
 import isAlpha from "validator/lib/isAlpha";
 
+/*
+ * @categorty type
+ * @since 1.0.0
+ */
 interface AlphaBrand {
   readonly Alpha: unique symbol;
 }
 
+/*
+ * @categorty type
+ * @since 1.0.0
+ */
 type Alpha = t.Branded<string, AlphaBrand>;
+
+/*
+
+ * A string that only includes alpha characters. E.g. a, b, c, A, B, C
+ *
+ * @categorty codec
+ * @since 1.0. 0
+ */
 const Alpha = t.brand(t.string, (s): s is Alpha => isAlpha(s), "Alpha");
 
 export { Alpha, AlphaBrand };
